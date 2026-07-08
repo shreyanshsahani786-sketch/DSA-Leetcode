f76267576a241a5a522215945fd40e4a984f9a14
Shreyansh Sahani <shreyanshsahani786@gmail.com>
2026-07-08 16:38:31 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.86%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
 
+        //base case
         if (n == 1)
         return true;
-        
+
+        // not valid number
         if (n <= 0)
             return false;
-        
+        // not divisible by 3
         if(n % 3 != 0)
         return false;
         
+        // recursive call krenge
         return isPowerOfThree(n/3);
     }
 };
