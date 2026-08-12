03d8faa5be11922c6efd54ac26eb2cc66318ee2e
Shreyansh Sahani <shreyanshsahani786@gmail.com>
2026-08-12 05:34:32 +0530

    Time: 3 ms (65.92%), Space: 10 MB (70.73%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        if(s1.size() > s2.size())
+            return false;
+
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+
+        // Frequency of s1
+        for(char ch : s1)
+            freq1[ch-'a']++;
+
+        int k = s1.size();
+
+        // First window
+        for(int i=0;i<k;i++)
+            freq2[s2[i]-'a']++;
+
+        if(freq1 == freq2)
+            return true;
+
+        // Slide window
+        for(int i=k;i<s2.size();i++){
+
+            freq2[s2[i]-'a']++;
+
+            freq2[s2[i-k]-'a']--;
+
+            if(freq1 == freq2)
+                return true;
+        }
+
+        return false;
+    }
+};
