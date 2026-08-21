043893b4235c83b541724a1a67c7b1af67aa437e
Shreyansh Sahani <shreyanshsahani786@gmail.com>
2026-08-22 02:48:57 +0530

    Time: 0 ms (100%), Space: 30.2 MB (87.46%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,12 +2,30 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
 
-    for(int i = 0; i < nums.size(); i++)
-    nums[i] = nums[i] * nums[i];
+        int n = nums.size();
 
-sort(nums.begin(), nums.end());
+        vector<int> ans(n);
 
-return nums;
-        
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                ans[index] = leftSquare;
+                left++;
+            } else {
+                ans[index] = rightSquare;
+                right--;
+            }
+
+            index--;
+        }
+
+        return ans;
     }
 };
