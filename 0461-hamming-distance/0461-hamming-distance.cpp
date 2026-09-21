575b3297a6cd610eab32d2c395a0ca47ba4c6980
Shreyansh Sahani <shreyanshsahani786@gmail.com>
2026-09-22 04:22:03 +0530

    Time: 0 ms (100%), Space: 8 MB (35.17%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -5,11 +5,15 @@ public:
         int n = x ^ y;
         int count = 0;
 
-        while(n !=0){
+        while (n > 0) {
 
-            n = n & (n -1);
-            count ++;
+            if (n & 1) {
+                count++;
+            }
+
+            n = n >> 1;
         }
-        return count;
+
+        return count;;
     }
 };
